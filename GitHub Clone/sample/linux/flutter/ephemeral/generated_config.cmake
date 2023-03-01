2a716814f450338c32cd63174822db6f5ef52798
Shaik Azad <120930148+Azad99-9@users.noreply.github.com>
2023-03-01 21:33:52 +0530

    Add files via upload

---
+++ GitHub Clone/sample/linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,20 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/student/snap/flutter/common/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/student/Desktop/sample" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/student/snap/flutter/common/flutter"
+  "PROJECT_DIR=/home/student/Desktop/sample"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=true"
+  "PACKAGE_CONFIG=/home/student/Desktop/sample/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=lib/main.dart"
+)
